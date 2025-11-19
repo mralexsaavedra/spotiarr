9cfaf7912f70fcaf54e7f257286fac06399f6d4a
Alexander Saavedra <mralexsaavedra@gmail.com>
2025-11-19 18:35:00 +0100

    chore: remove Bulma styles and add Tailwind CSS configuration

---
+++ src/frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'hsl(141, 73%, 42%)',
+          light: 'hsl(141, 73%, 52%)',
+          dark: 'hsl(141, 73%, 32%)',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
