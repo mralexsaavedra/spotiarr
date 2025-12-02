1623dc520ef51bf2d42cdfcf8e7f312e6c9fb2ff
Alexander Saavedra <mralexsaavedra@gmail.com>
2025-12-02 09:57:53 +0100

    feat: implement lazy loading for all route views with a Suspense fallback.

--- src/frontend/src/routes/Routing.tsx
+++ src/frontend/src/routes/Routing.tsx
@@ -1,15 +1,32 @@
-import { FC } from "react";
+import { FC, Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
+import { Loading } from "../components/atoms/Loading";
 import { AppLayout } from "../components/layouts/AppLayout";
-import { ArtistDetail } from "../views/ArtistDetail";
-import { Artists } from "../views/Artists";
-import { History } from "../views/History";
-import { Home } from "../views/Home";
-import { PlaylistDetail } from "../views/PlaylistDetail";
-import { PlaylistPreview } from "../views/PlaylistPreview";
-import { Releases } from "../views/Releases";
-import { Settings } from "../views/Settings";
 import { Path } from "./routes";
+
+// Lazy load views
+const Home = lazy(() => import("../views/Home").then((module) => ({ default: module.Home })));
+const PlaylistDetail = lazy(() =>
+  import("../views/PlaylistDetail").then((module) => ({ default: module.PlaylistDetail })),
+);
+const PlaylistPreview = lazy(() =>
+  import("../views/PlaylistPreview").then((module) => ({ default: module.PlaylistPreview })),
+);
+const History = lazy(() =>
+  import("../views/History").then((module) => ({ default: module.History })),
+);
+const Releases = lazy(() =>
+  import("../views/Releases").then((module) => ({ default: module.Releases })),
+);
+const Artists = lazy(() =>
+  import("../views/Artists").then((module) => ({ default: module.Artists })),
+);
+const ArtistDetail = lazy(() =>
+  import("../views/ArtistDetail").then((module) => ({ default: module.ArtistDetail })),
+);
+const Settings = lazy(() =>
+  import("../views/Settings").then((module) => ({ default: module.Settings })),
+);
 
 interface RoutingProps {
   pathname: Path;
@@ -17,16 +34,18 @@ interface RoutingProps {
 }
 
 export const Routing: FC<RoutingProps> = ({ pathname, version }) => (
-  <Routes>
-    <Route element={<AppLayout pathname={pathname} version={version} />}>
-      <Route path={Path.HOME} element={<Home />} />
-      <Route path={Path.PLAYLIST_DETAIL} element={<PlaylistDetail />} />
-      <Route path={Path.PLAYLIST_PREVIEW} element={<PlaylistPreview />} />
-      <Route path={Path.HISTORY} element={<History />} />
-      <Route path={Path.RELEASES} element={<Releases />} />
-      <Route path={Path.ARTISTS} element={<Artists />} />
-      <Route path={Path.ARTIST_DETAIL} element={<ArtistDetail />} />
-      <Route path={Path.SETTINGS} element={<Settings />} />
-    </Route>
-  </Routes>
+  <Suspense fallback={<Loading />}>
+    <Routes>
+      <Route element={<AppLayout pathname={pathname} version={version} />}>
+        <Route path={Path.HOME} element={<Home />} />
+        <Route path={Path.PLAYLIST_DETAIL} element={<PlaylistDetail />} />
+        <Route path={Path.PLAYLIST_PREVIEW} element={<PlaylistPreview />} />
+        <Route path={Path.HISTORY} element={<History />} />
+        <Route path={Path.RELEASES} element={<Releases />} />
+        <Route path={Path.ARTISTS} element={<Artists />} />
+        <Route path={Path.ARTIST_DETAIL} element={<ArtistDetail />} />
+        <Route path={Path.SETTINGS} element={<Settings />} />
+      </Route>
+    </Routes>
+  </Suspense>
 );
