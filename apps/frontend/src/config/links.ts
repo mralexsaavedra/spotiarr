6e184d0a412f6639ebe37ef6fe75f50bfcac972c
Alexander Saavedra <mralexsaavedra@gmail.com>
2025-12-15 10:31:39 +0100

    feat: Add navigation, app settings, version, and external links configurations, and update .gitignore.

---
+++ apps/frontend/src/config/links.ts
@@ -0,0 +1,4 @@
+import { APP_VERSION } from "./version";
+
+export const BUY_ME_A_COFFEE_URL = "https://buymeacoffee.com/mralexsaavedra";
+export const GITHUB_RELEASE_URL = `https://github.com/mralexsaavedra/spotiarr/releases/tag/v${APP_VERSION}`;
